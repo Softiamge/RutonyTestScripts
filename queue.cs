5848b65456665979b6defc2936da61b0a1ad6ed2
DoshiPanda <69212711+Softiamge@users.noreply.github.com>
2024-04-27 00:38:57 +0500

    Add files via upload

--- queue.cs
+++ queue.cs
@@ -8,14 +8,31 @@ namespace RutonyChat {
        
         public Queue<string> queue = new Queue<string>();
 		public bool _state = false;
-		public string CommandFT = "!ft";
+		public string Comm = "!ft";
 		public string Phrase_start = "Стример принимает заявки на FT3";
 		public string Phrase_stop = "Стример не принимает заявки";
-		public string Phrase_pl_add = "$name ты добавлен в очередь, твой номер $place";
+        public string Phrase_mt = "В очереди никого не осталось";
+        public string Phrase_cnt = "В очереди $cnt человек";
+
+
+        public string Phrase_pl_add = "$name ты добавлен в очередь, твой номер $place";
 		public string Phrase_pl_wl = "$name, Спасибо за игру";
 		public string Phrase_pl_del = "$name исключен из очереди";
-        
 
+        public void NewAlert(string site, string typeEvent, string subplan, string name, string text, float donate, string currency, int qty)
+        {
+
+            if (typeEvent == "donate")
+            {
+                RutonyBot.BotSay(site, name + " поддержал на " + donate.ToString() + currency);
+            }
+
+            if (typeEvent == "TwitchPoints")
+            {
+                RutonyBot.BotSay(site, name + " активировал награду за " + donate.ToString());
+            }
+
+        }
 
 
         public void InitParams(string param) {
@@ -25,68 +42,105 @@ namespace RutonyChat {
         public void Closing() {
             RutonyBot.SayToWindow("Очередь на фт отключена");
         }
-
-        public void NewMessage(string site, string name, string text, bool system) 
-		{
-            RutonyBot.SayToWindow(string.Format("site={0}, name={1}, text={2}", site, name, text));
-            RutonyBot.SayToWindow("NewMessage()");
-        }
-        public void NewMessageEx(string site, string name, string text, bool system, Dictionary<string, string> Params) 
-		{
-            
-            if(text.StartsWith("!ft"))
-            {  RutonyBot.BotSay(site, "entered !ft"); }
-            
-            if (Equals(null, Params))
-            {
-				RutonyBot.BotSay(site, "Params is null!");
-            }
-            else
-            {
-				foreach (var item in Params)
-				{
-					RutonyBot.BotSay(site,item.Key);
-					RutonyBot.BotSay(site,item.Value);
-				}
-			}
-
-            if(text == "!ft" ) 
-            {
-                if(_state)
-                { RutonyBot.BotSay(site, name + " ты можешь подать заявку командой !ft3"); }
-                else 
-                { RutonyBot.BotSay(site, Phrase_stop);  return;}
-
-            } 
-            
-        }
-
-        public void FT(string name, string text) 
+        public void NewMessage(string site, string name, string text, bool system = false, Dictionary<string, string> Params = null)
         {
-            string[] text_arr = text.Split(' ');
+            NewMessageEx(site, name, text, system, Params);
+        }
 
-            if(text_arr.Length < 2)
+            public void NewMessageEx(string site, string name, string text, bool system, Dictionary<string, string> Params) 
+		{
 
-            if (_state)
-            { }
-            else
-            {
-                if(text_arr[1] == "start" && !RutonyBot.ListAdmins.Contains(name) ) 
+            if (text.StartsWith(Comm))
+            { 
+                var msg = text.Substring(Comm.Length-1);
+                bool adm = false;
+
+                if (ProgramSettings.twitch.Channel == name || RutonyBot.ListAdmins.Contains(name)) { adm = true; }
+                RutonyBot.SayToWindow(msg);
+
+                if (text == "!ft")
                 { 
-                    _state = true;
-                    RutonyBot.SayToWindow(Phrase_start);
+                    if (_state)
+                    { 
+                        RutonyBot.BotSay(site, Phrase_start);
+                        RutonyBot.BotSay(site, "чтобы принять участие напиши !ft3");
+                    }
+                    else 
+                    { RutonyBot.BotSay(site, Phrase_stop); }
+                }    
+
+                if (msg == "start")
+                {
+                    if (adm)
+                    { _state = true; }
+                    else 
+                    { RutonyBot.BotSay(site, "У вас не достаточно прав"); }
                 }
-                
-            
+
+                if (msg == "stop")
+                {
+                    if (adm)
+                    { _state = true; }
+                    else
+                    { RutonyBot.BotSay(site, "У вас не достаточно прав"); }
+                }
+
+                if (msg == "3")
+                {
+                    if (_state)
+                    { 
+                        FT3(name);
+                    }
+                    else
+                    { RutonyBot.BotSay(site, Phrase_stop); }
+                }
+
+                if (msg == "next" && adm )
+                {
+                    if (queue.Count > 0)
+                    {
+                        RutonyBot.BotSay(site, Phrase_pl_wl.Replace("$name", queue.Peek()));
+                        queue.Dequeue();
+                        RutonyBot.BotSay(site, Phrase_cnt.Replace("$cnt", queue.Count.ToString()));
+                    }
+                    else
+                    {
+                        RutonyBot.BotSay(site, Phrase_mt);
+                    }
+                }
+
+                if (msg == "del" && adm)
+                {
+                    if (queue.Count > 0)
+                    {
+                        RutonyBot.BotSay(site, Phrase_pl_del.Replace("$name", queue.Peek()));
+                        queue.Dequeue();
+                        RutonyBot.BotSay(site, Phrase_cnt.Replace("$cnt", queue.Count.ToString()));
+                    }
+                    else
+                    {
+                        RutonyBot.BotSay(site, Phrase_mt);
+                    }
+                }
+
+
+
+
             }
+                    
+            
+             
             
         }
+
+        public void NewAlert() { }
+
 
         public void FT3(string name)
         { 
             queue.Enqueue(name);
 
-            RutonyBot.SayToWindow(Phrase_start.Replace("$name",name));
+            RutonyBot.SayToWindow(Phrase_pl_add.Replace("$name",name).Replace("$place", queue.Count.ToString()));
 
         }
     }
